0cb8d255a3fd8c93330834fc5726dc371e2269ae
Henrique <cerejahenrique@gmail.com>
2024-08-02 18:24:30 -0300

    feat: Home page total balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Henrique" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Bem-Vindo"
+            user={loggedIn?.firstName || "Visitante"}
+            subtext="Acesse e controle sua conta e transações eficientemente"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
