2a18e721965627c928fb7953cbd6698424476920
Henrique <cerejahenrique@gmail.com>
2024-08-03 15:02:27 -0300

    feat: create left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,33 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Henrique", lastName: "Cereja" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            alt="Ícone Menu"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { Chart as ChartJs, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
